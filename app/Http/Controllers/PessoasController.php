da3028141b0a1e1766b76c386fe46aa3b4ec5579
Lainez <lainezlsaraujo@gmail.com>
2018-12-27 12:06:48 -0300

    Injeção de depências

--- app/Http/Controllers/PessoasController.php
+++ app/Http/Controllers/PessoasController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Pessoa;
+use App\Telefone;
 use Illuminate\Http\Request;
 
 class PessoasController extends Controller
 {
+
+    //injeção de dependência
+    private $telefones_controller;
+
+    public function __construct(TelefonesController $telefones_controller)
+    {
+        $this->telefones_controller = $telefones_controller;
+    }
+
     //método para retorna pessoas
     public function index()
     {
@@ -21,8 +31,16 @@ class PessoasController extends Controller
         return view('pessoas.create');
     }
 
-    public function store(Request $request){
-        Pessoa::create($request->all());
-        return redirect("/pessoas")->with("message","Pessoa criada com sucesso");
+    public function store(Request $request)
+    {
+       $pessoa = Pessoa::create($request->all());
+        if ($request->ddd && $request->telefone) {
+            $telefone = new Telefone();
+            $telefone->ddd = $request->ddd;
+            $telefone->telefone = $request->telefone;
+            $telefone->pessoa_id = $pessoa->id;
+            $this->telefones_controller->store($telefone);
+        }
+       return redirect("/pessoas")->with("message", "Pessoa criada com sucesso");
     }
 }
