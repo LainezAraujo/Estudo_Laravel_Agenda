aeb3e9a32c6bfc42ad142aabc90fc1db4e6672b4
Lainez <lainezlsaraujo@gmail.com>
2018-12-27 18:32:48 -0300

    Validação nome, telefone,ddd

--- app/Http/Controllers/PessoasController.php
+++ app/Http/Controllers/PessoasController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Pessoa;
 use App\Telefone;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PessoasController extends Controller
 {
@@ -16,7 +17,7 @@ class PessoasController extends Controller
     public function __construct(TelefonesController $telefones_controller)
     {
         $this->telefones_controller = $telefones_controller;
-        $this->pessoa= new Pessoa();
+        $this->pessoa = new Pessoa();
     }
 
     //método para retorna pessoas
@@ -35,21 +36,31 @@ class PessoasController extends Controller
 
     public function store(Request $request)
     {
-       $pessoa = Pessoa::create($request->all());
+
+        $validacao = $this->validacao($request->all());
+
+        if ($validacao->fails()) {
+            return redirect()->back()
+                ->withErrors($validacao->errors())
+                ->withInput($request->all());
+        }
+
+        $pessoa = Pessoa::create($request->all());
         if ($request->ddd && $request->telefone) {
+
             $telefone = new Telefone();
             $telefone->ddd = $request->ddd;
             $telefone->telefone = $request->telefone;
             $telefone->pessoa_id = $pessoa->id;
             $this->telefones_controller->store($telefone);
         }
-       return redirect("/pessoas")->with("message", "Pessoa criada com sucesso");
+        return redirect("/pessoas")->with("message", "Pessoa criada com sucesso");
     }
 
     public function excluirView($id)
     {
-        return view('pessoas.delete',[
-            'pessoa'=>$this->getPessoa($id)
+        return view('pessoas.delete', [
+            'pessoa' => $this->getPessoa($id)
         ]);
     }
 
@@ -57,22 +68,55 @@ class PessoasController extends Controller
     {
         $this->getPessoa($id)->delete();
 
-        return redirect(url('pessoas'))->with('success','Excluído!');
+        return redirect(url('pessoas'))->with('success', 'Excluído!');
     }
 
     public function editarView($id)
     {
-        return view('pessoas.edit',[
-           'pessoa'=>$this->getPessoa($id)
+        return view('pessoas.edit', [
+            'pessoa' => $this->getPessoa($id)
         ]);
     }
-    public function update(Request $request){
+
+    public function update(Request $request)
+    {
+        $validacao = $this->validacao($request->all());
+
+        if ($validacao->fails()) {
+            return redirect()->back()
+                ->withErrors($validacao->errors())
+                ->withInput($request->all());
+        }
+
         $pessoa = $this->getPessoa($request->id);
         $pessoa->update($request->all());
 
         return redirect('/pessoas');
     }
-    protected function getPessoa($id){
+
+    protected function getPessoa($id)
+    {
         return $this->pessoa->find($id);
+    }
+
+    private function validacao($data)
+    {
+        if (array_key_exists('ddd', $data) && array_key_exists('telefone', $data)) {
+            if ($data['ddd'] || $data['telefone']) {
+                $regras['ddd'] = 'required|size:2';
+                $regras['telefone'] = 'required';
+            }
+        }
+        $regras['nome'] = 'required|min:3';
+
+        $mensagens = [
+            'nome.required' => 'Campo nome é obrigatório',
+            'nome.min' => 'Campo nome deve possuir ao menos 3 letras',
+            'ddd.required' => 'Campo ddd é obrigatório',
+            'ddd.size' => 'Campo ddd deve ter 2 dígitos',
+            'telefone.required' => 'Campo telefone é obrigatório'
+        ];
+
+        return Validator::make($data, $regras, $mensagens);
     }
 }
